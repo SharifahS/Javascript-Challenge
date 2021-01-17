84aaa83c8a60c17a11ceeb5b810a10c22bdc6735
SharifahS <sharifah.s.sinclair@gmail.com>
2021-01-16 20:46:56 -0500

    commit filter button

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,5 +1,6 @@
 // from data.js
 var tableData = data;
+var dataMatch = null;
 
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
@@ -7,7 +8,7 @@ var tbody = d3.select("tbody");
 //loop through data and add 1 table row for each ufo sigthing
 //use object entries for ufo value
 //append 1 cell per ufo value and update each cell
-data.forEach((UFO) => {
+var table = data.forEach((UFO) => {
     var row = tbody.append("tr");
     Object.entries(UFO).forEach(([key, value]) => {
       var cell = row.append("td");
@@ -15,4 +16,69 @@ data.forEach((UFO) => {
     });
 
 });
+
+// Submit Button handler
+function handleSubmit() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    if(UFO){
+        filter = 1;
+        matchData = tableData.filter(obs => obs.datetime == UFO);
+        tbody.html("");
+        matchData.forEach(row => {
+            tbody.append("tr");
+            for (key in row){
+                var cell = tbody.append("td");
+                cell.text(row[key]);
+            }
+        });
+    };
+  
+    // Select the input value from the form
+    var UFO = d3.select("#datetime").node().value;
+
+    // clear the input value
+   var dateEnter =  d3.select("#datetime").node().value = "";
+
+    var dateEnter = date.property("value");
+  
+  
+    // Build the plot with the new stock
+    table(UFO);
+
+    function returnData(filtered_val){
+        if (filtered_val){
+            return dataMatch;
+        } else {
+            return tableData;
+        }
+    };
+
+
+}
+
+  
+  d3.select("#filter-btn").on("click", handleSubmit())
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
