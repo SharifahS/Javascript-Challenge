06362f87d5f2c86cf5b04012d888603287f13010
SharifahS <sharifah.s.sinclair@gmail.com>
2021-01-16 19:16:04 -0500

    commit table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,17 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+//loop through data and add 1 table row for each ufo sigthing
+//use Object.entries for ufo value
+//append 1 cell per ufo value and update each cell with ufo values
+data.forEach((UFO) => {
+    var row = tbody.append("tr");
+    Object.entries(UFO).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+
+});
+
